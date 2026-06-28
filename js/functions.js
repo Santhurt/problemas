1e09ee2001591a736a0b205403981cc797e00496
santhurt <santiruizhurt@gmail.com>
2026-06-27 23:08:36 -0500

    solucion addTwoNumbers js v1

--- js/functions.js
+++ js/functions.js
@@ -88,27 +88,25 @@ export const Problems = {
      * @returns {ListNode}
      */
     addTwoNumbers: function (l1, l2) {
-        let result = new ListNode();
-        const head = result;
+        let dummy = new ListNode();
+        let tail = dummy;
         let carry = 0;
 
-        while (l1 || l2) {
-            let sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0);
-            result.val = (sum + carry) % 10;
-            carry = Math.floor((sum + carry) / 10);
+        while (l1 || l2 || carry) {
+            let x = l1 ? l1.val : 0;
+            let y = l2 ? l2.val : 0;
 
-            l1 = l1 ? l1.next : l1;
-            l2 = l2 ? l2.next : l2;
+            let sum = x + y + carry;
+            carry = Math.floor(sum / 10);
 
-            result.next =
-                !l1 && !l2
-                    ? carry != 0
-                        ? new ListNode(carry, null)
-                        : null
-                    : new ListNode();
-            result = result.next;
+            let sumNode = new ListNode(sum % 10, null);
+            tail.next = sumNode;
+
+            tail = tail.next;
+            l1 = l1.next;
+            l2 = l2.next;
         }
 
-        head.print();
+        dummy.print();
     },
 };
